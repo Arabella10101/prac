27facf4fd7d8892c3b7cdd2e7cf3648d09935ed5
Arabella <arabella2110@gmail.com>
2019-09-05 12:14:31 +0200

    added more experimental bootstrap

--- scipt.js
+++ scipt.js
@@ -2,5 +2,11 @@
 
 var but= document.getElementById("one");
 but.addEventListener("click",function(){
-    but.style.backgroundColor="red"
+    but.style.backgroundColor="red";
+})
+
+var btndark = document.getElementById("dark1");
+btndark.addEventListener("click", function(){
+    this.classList.remove("btn-outline-dark");
+    this.classList.add("btn-outline-primary");
 })
