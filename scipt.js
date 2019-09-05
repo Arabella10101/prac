ef7cc2e4ae11d97217e9e164a53e478c7bea742e
Arabella <arabella2110@gmail.com>
2019-09-05 12:40:54 +0200

    linked about.html to index.html

--- scipt.js
+++ scipt.js
@@ -5,8 +5,14 @@ but.addEventListener("click",function(){
     but.style.backgroundColor="red";
 })
 
-var btndark = document.getElementById("dark1");
-btndark.addEventListener("click", function(){
-    this.classList.remove("btn-outline-dark");
-    this.classList.add("btn-outline-primary");
-})
+// var btndark = document.getElementById("dark1");
+// btndark.addEventListener("click", function(){
+//     this.classList.remove("btn-outline-dark");
+//     this.classList.add("btn-outline-primary");
+// })
+
+function goDark() {
+    var btndark = document.getElementById("dark1");
+    btndark.classList.remove("btn-outline-dark");
+    btndark.classList.add("btn-outline-primary");
+}
